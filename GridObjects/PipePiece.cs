dbdfc751dd57c2759a78939b7413ac12fb2839ef
ConstantSeal592 <reubensmith@outlook.com>
2025-08-03 13:00:56 +0100

    waste pipe colouration

--- GridObjects/PipePiece.cs
+++ GridObjects/PipePiece.cs
@@ -10,4 +10,14 @@ public partial class PipePiece : Node2D, Pipe {
     public int Cost { get; set; }
     public bool HasPushedWater { get; set; } = false;
     public bool IsPureWater { get; set; } = true;
+
+    public override void _Process(double delta) {
+        var texture = GetNode<TextureRect>("TextureRect");
+        if (!IsPureWater) {
+            texture.Modulate = new Color(0.75f, 0.75f, 0.75f);
+        }
+        else {
+            texture.Modulate = new Color(1f, 1f, 1f);
+        }
+    }
 }
